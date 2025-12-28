73cc68bd5c7cc06d0e20bd407c6d760057e7b6ad
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-12-28 23:54:32 +0530

    completed dynamic nested routing

--- Section 2 (Routing)/routing-next/src/app/blogs/[blog]/comments/[commentID]/page.js
+++ Section 2 (Routing)/routing-next/src/app/blogs/[blog]/comments/[commentID]/page.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default async function Comment({ params }) {
+export default async function Comments({ params }) {
   console.log(await params);
   const { blog } = await params;
   return (
     <div>
-      <p>all comments on : {blog} page</p>
+      <p>all comments on comments page to commentID : {blog} id</p>
     </div>
   );
 }
