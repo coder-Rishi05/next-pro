3ff5108a928c30ca196c0e3647d58cc477ea57ea
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-12-29 14:54:58 +0530

    completed custom error

--- Section 2 (Routing)/routing-next/src/app/blogs/[blog]/page.js
+++ Section 2 (Routing)/routing-next/src/app/blogs/[blog]/page.js
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 export const metadata = {
@@ -7,9 +8,20 @@ export const metadata = {
 async function page1({ params }) {
   console.log(await params);
   const { blog } = await params;
+  // if blog is === test
+  // if (blog === "test") {
+  //   return notFound();
+  // }
+
+  // if blog is not number
+  if (!/^\d+$/.test(blog)) {
+    return notFound();
+  }
+
   return (
     <div>
       <h1>This is child blog1</h1>
+
       <p>Blog id : {blog}</p>
     </div>
   );
---
+++ Section 2 (Routing)/routing-next/src/app/not-found.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1>This is my custom not found page.</h1>
+    </div>
+  );
+};
